3bb5aa2e6eaf1b43ab41fe1ad561cfd79dea4c99
hongjun423 <hongjc423@naver.com>
2019-12-27 23:06:29 +0900

    aop 이용한 controller 로거 추가

---
+++ src/main/java/com/hongjun423/toy_project/config/ControllerLogger.java
@@ -0,0 +1,66 @@
+package com.hongjun423.toy_project.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Aspect
+@Component
+public class ControllerLogger {
+
+    @Pointcut("execution(* com.hongjun423.toy_project.controller.*.*(..))")
+    public void controllerPointcut() {
+    }
+
+    @Around("controllerPointcut()")
+    public Object doLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        String controllerName = joinPoint.getSignature().getDeclaringType().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Map<String, Object> params = new HashMap<>();
+
+        try {
+            params.put("controller", controllerName);
+            params.put("method", methodName);
+            params.put("params", getParams(request));
+            params.put("log_time", LocalDateTime.now());
+            params.put("request_uri", request.getRequestURI());
+            params.put("http_method", request.getMethod());
+        } catch (Exception e) {
+            log.error("LoggerAspect error", e);
+        }
+
+        log.info("controller - {}", params);
+        Object result = joinPoint.proceed();
+
+        return result;
+
+    }
+
+    private static JSONObject getParams(HttpServletRequest request) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            String replaceParam = param.replaceAll("\\.", "-");
+            jsonObject.put(replaceParam, request.getParameter(param));
+        }
+        return jsonObject;
+    }
+}
--- src/main/java/com/hongjun423/toy_project/controller/SampleController.java
+++ src/main/java/com/hongjun423/toy_project/controller/SampleController.java
@@ -1,10 +1,19 @@
-package com.hongjun423.toy_project.Sample;
+package com.hongjun423.toy_project.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class SampleController {
+
+    @GetMapping("/")
+    public String main() {
+        log.info("main page in");
+        return "main page";
+    }
+
 
     @GetMapping("/hello")
     public String hello() {
